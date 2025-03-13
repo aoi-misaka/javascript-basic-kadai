415890589155f635949e422a7dd1ecabbd88b411
aoi-misaka <aoimisaka.05@gmail.com>
2025-03-13 19:02:13 +0900

    訂正3

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,14 +4,14 @@ if (num % 3 === 0 && num % 5 === 0) {
   console.log ('3と5の倍数です');
 }
 
-/*if (num % 3 === 0){
+else if (num % 3 === 0){
   console.log('3の倍数です')
 }
 
-if (num % 5 === 0){
+else if (num % 5 === 0){
   console.log('5の倍数です')
-}*/
+}
 
 else{
-  console.log (1,2,4,7,8,11,13,14);
+  console.log (num);
 }
