d83e651f96f2df06c0a66c8b620ea85923653cde
aoi-misaka <aoimisaka.05@gmail.com>
2025-03-29 02:00:14 +0900

    修正

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,3 +1,4 @@
+console.log(document.getElementById('text'));
 document.getElementById('btn').addEventListener('click', function() {
   document.getElementById('text').textContent = 'ボタンをクリックしました';
 });
